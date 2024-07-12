2c3bf5cdda91d8aff5c949c55c612c8b6019d469
code-qtzl <azepeda90@outlook.com>
2024-07-11 21:14:35 -0500

    oop ch1: line break and double quote to single quote

--- Ch-1/dry.py
+++ Ch-1/dry.py
@@ -1,6 +1,7 @@
 def fight_soldiers(soldier_one, soldier_two):
     soldier_one_dps = get_soldier_dps(soldier_one)
     soldier_two_dps = get_soldier_dps(soldier_two)
+
     if soldier_one_dps > soldier_two_dps:
         return "soldier 1 wins"
     if soldier_two_dps > soldier_one_dps:
@@ -9,5 +10,5 @@ def fight_soldiers(soldier_one, soldier_two):
 
 
 def get_soldier_dps(soldier):
-    return soldier['damage'] * soldier["attacks_per_second"]
+    return soldier['damage'] * soldier['attacks_per_second']
     
